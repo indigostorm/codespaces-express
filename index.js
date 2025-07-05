43c3c0061c828af562746666afd134b3b46f21c7
indigostorm <vpetit@3dplanet.net>
2025-07-04 18:43:11 -0700

    Update index.js
    added semicolons

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 //const port = process.env.Port || 3000
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get('/test',(req, res) => {
   res.send('This is only a Test!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
