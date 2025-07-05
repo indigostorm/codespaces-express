b0620b01efc67080ed90a8c5347def4840cc9b68
indigostorm <vpetit@3dplanet.net>
2025-07-04 18:05:44 -0700

    Update index.js
    added a test path

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/test',(req, res) => {
+  res.send('This is only a Test!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
